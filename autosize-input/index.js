3925b99a59929f1a92dc83d34776f5adde5ffeca
PolinaVP <polina_vp@tut.by>
2020-06-26 16:21:21 +0300

    fixes

--- autosize-input/index.js
+++ autosize-input/index.js
@@ -132,8 +132,8 @@ $(window).on('load', function () {
   autosizeInput(document.querySelector('#task_company'));
   autosizeInput(document.querySelector('#task_email'));
   autosizeInput(document.querySelector('#task_msg'));
-  autosizeInput(document.querySelector('#task_source'));
-  autosizeInput(document.querySelector('#task_purpose'));
+  // autosizeInput(document.querySelector('#task_source'));
+  // autosizeInput(document.querySelector('#task_purpose'));
 
 });
 
@@ -142,8 +142,8 @@ $(window).on('resize', function () {
   autosizeInput(document.querySelector('#task_company'));
   autosizeInput(document.querySelector('#task_email'));
   autosizeInput(document.querySelector('#task_msg'));
-  autosizeInput(document.querySelector('#task_source'));
-  autosizeInput(document.querySelector('#task_purpose'));
+  // autosizeInput(document.querySelector('#task_source'));
+  // autosizeInput(document.querySelector('#task_purpose'));
 
 });
 

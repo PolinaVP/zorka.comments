ebd2c2842f342c2a8268b39926d86000a5d76d21
PolinaVP <polina_vp@tut.by>
2020-07-06 10:39:08 +0300

    fixes

--- main.js
+++ main.js
@@ -136,6 +136,8 @@ $('span','.select_ar').on('click', function() {
 $(document).on('click touch', '.select_ar ul li', function(e){
     e.preventDefault();
     let dropdown = $(this).parent().parent();
+    let active = dropdown.find($('option').text($(this).text()));
+    active.prop('selected', true);
     dropdown.removeClass('open');
 })
 

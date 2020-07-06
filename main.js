6b3ad88143b5cfe815a53386f29f8894cfe02d34
PolinaVP <polina_vp@tut.by>
2020-07-06 11:07:10 +0300

    fixes

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ $(document).on('click touch', '.select_ar ul li', function(e){
     dropdown.find('option:selected').removeAttr('selected');
     let text = $(this).text();
     let active = dropdown.find($(`option:contains(${text})`));
-    active.attr('selected');
+    active.attr('selected', true);
     dropdown.removeClass('open');
 })
 

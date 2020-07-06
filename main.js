f97c412071257c5576fee7e289ebee3ab0e83e6d
PolinaVP <polina_vp@tut.by>
2020-07-06 13:23:11 +0300

    fixes

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ $('#influencer').click(function () {
 window.addEventListener('load', function () {
     var forms = document.getElementsByClassName('needs-validation');
     var validation = Array.prototype.filter.call(forms, function (form) {
-      $('.send_msg').on('click', function (event) {
+      $('form.open ~ .btn_smile').on('click', function (event) {
           alert(123);
         if (form.checkValidity() === false) {
           event.preventDefault();

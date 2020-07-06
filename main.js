113106aaa5481f3cd432ec77df9ad93528037bf3
PolinaVP <polina_vp@tut.by>
2020-07-06 13:18:35 +0300

    fixes

--- main.js
+++ main.js
@@ -38,8 +38,7 @@ $('#influencer').click(function () {
 })
 
 
-(function () {
-    'use strict';
+
 window.addEventListener('load', function () {
     var forms = document.getElementsByClassName('needs-validation');
     var validation = Array.prototype.filter.call(forms, function (form) {
@@ -65,8 +64,7 @@ window.addEventListener('load', function () {
         form.classList.add('was-validated');
       }, false);
     });
-  }, false);
-})();
+  }
 
 
 
